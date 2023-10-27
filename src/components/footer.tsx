520e8578664ec05be3717360dbf905d3e0f6d981
Jongun Jeong <skeep194@knu.ac.kr>
2023-10-27 22:26:24 +0900

    add footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,48 +2,28 @@ import React from 'react';
 import { Link } from 'gatsby';
 
 const Footer: React.FC = () => {
-  // .Site.Sections에 해당하는 데이터. 실제로는 이 데이터를 동적으로 가져와야 합니다.
   const sections = [
     { name: 'Section1', rssUrl: '/rss-section1' },
     { name: 'Section2', rssUrl: '/rss-section2' },
-    // ...
   ];
 
-  // .Site.Params.footer_rows에 해당하는 데이터. 실제로는 이 데이터를 동적으로 가져와야 합니다.
   const footerRows = [
     {
       items: [
         { pre: '<strong>Pre Item</strong>', url: '#', name: 'Item1' },
-        { url: '/item2', name: 'Item2' },
-        // ...
+        { url: '/item2', name: 'Item2' }, 
       ],
     },
-    // ...
   ];
 
   return (
     <footer className="footer">
       <div className="footer-row">
-        {sections.map((section) => (
-          <Link className="footer-item" key={section.name} to={section.rssUrl}>
-            Feed of {section.name} <i className="icofont-rss"></i>
-          </Link>
-        ))}
+        Copyright 2023 ©<Link to="https://github.com/skeep194">skeep194</Link>, built with&nbsp;<Link to="https://github.com/kasterra">kasterra</Link>
       </div>
-
-      {footerRows.map((row, idx) => (
-        <div className="footer-row" key={idx}>
-          {row.items.map((item, idx) => (
-            item.pre ? (
-              <span className="footer-item" key={idx} dangerouslySetInnerHTML={{ __html: item.pre }} />
-            ) : (
-              <Link className="footer-item" key={idx} to={item.url}>
-                {item.name}
-              </Link>
-            )
-          ))}
-        </div>
-      ))}
+      <div className="footer-row">
+        from&nbsp;<Link to="https://github.com/leafee98/hugo-theme-flat">hugo-theme-flat</Link>&nbsp;migrate gatsby
+      </div>
     </footer>
   );
 };
